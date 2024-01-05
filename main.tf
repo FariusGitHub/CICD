46810dd9f7f3c813cd124300b23b8c6ffcbf7454
FariusGitHub <ftjioesman@hotmail.com>
2024-01-05 15:21:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,6 +96,13 @@ resource "aws_security_group" "sg_api_project6" {
     cidr_blocks = ["0.0.0.0/0"]
   }   
 
+   ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
